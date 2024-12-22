ac32760565b52435799f4bebc3342fa0c1fed655
Tim Giertz <chewyc99@hotmail.com>
2024-12-22 13:05:10 -0600

    Adding l2 as well as bgp

--- apps/kustomize/update.sh
+++ apps/kustomize/update.sh
@@ -23,13 +23,37 @@ helm template \
     --set=bgpControlPlane.enabled=true \
     --set=hubble.relay.enabled=true \
     --set=hubble.ui.enabled=true \
-    --set=cni.exclusive=false > cilium/install-cilium.yaml
+    --set=cni.exclusive=false > cilium-bgp/install-cilium.yaml
 
-kustomize build ./cilium | yq -i 'with(.cluster.inlineManifests.[] | select(.name=="kube-system"); .contents=load_str("/dev/stdin"))' ../../patches/cilium.yaml
+# kustomize build ./cilium-bgp | yq -i 'with(.cluster.inlineManifests.[] | select(.name=="kube-system"); .contents=load_str("/dev/stdin"))' ../../patches/cilium.yaml
 
+helm template \
+    cilium \
+    cilium/cilium \
+    --version 1.16.4 \
+    --namespace kube-system \
+    --set ipam.mode=kubernetes \
+    --set=kubeProxyReplacement=true \
+    --set=securityContext.capabilities.ciliumAgent="{CHOWN,KILL,NET_ADMIN,NET_RAW,IPC_LOCK,SYS_ADMIN,SYS_RESOURCE,DAC_OVERRIDE,FOWNER,SETGID,SETUID}" \
+    --set=securityContext.capabilities.cleanCiliumState="{NET_ADMIN,SYS_ADMIN,SYS_RESOURCE}" \
+    --set=cgroup.autoMount.enabled=false \
+    --set=cgroup.hostRoot=/sys/fs/cgroup \
+    --set=k8sServiceHost=localhost \
+    --set=k8sServicePort=7445 \
+    --set=nodeIPAM.enabled=true \
+    --set=gatewayAPI.enabled=false \
+    --set=envoy.securityContext.capabilities.keepCapNetBindService=true \
+    --set=l2announcements.enabled=true \
+    --set=k8sClientRateLimit.qps=30 \
+    --set=k8sClientRateLimit.burst=50 \
+    --set=hubble.relay.enabled=true \
+    --set=hubble.ui.enabled=true \
+    --set=cni.exclusive=false > cilium-l2/install-cilium.yaml
+
+kustomize build ./cilium-l2 | yq -i 'with(.cluster.inlineManifests.[] | select(.name=="kube-system"); .contents=load_str("/dev/stdin"))' ../../patches/cilium.yaml
 
 helm template istio-base istio/base -n istio-system --create-namespace > ../../deployment/apps/istio/istio-base.yaml
-helm template istiod istio/istiod --namespace istio-system --set profile=ambient --set pilot.env.PILOT_ENABLE_ALPHA_GATEWAY_API=true > ../../deployment/apps/istio/istio-control-plane.yaml
+helm template istiod istio/istiod -n istio-system --set profile=ambient --set pilot.env.PILOT_ENABLE_ALPHA_GATEWAY_API=true > ../../deployment/apps/istio/istio-control-plane.yaml
 helm template istio-cni istio/cni -n istio-system --set profile=ambient > ../../deployment/apps/istio/istio-cni.yaml
 helm template ztunnel istio/ztunnel -n istio-system > ../../deployment/apps/istio/istio-ztunnel.yaml
 helm template istio-ingress istio/gateway -n istio-ingress --create-namespace  > ../../deployment/apps/istio/istio-gateway.yaml
